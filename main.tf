b7dcbd725c72dc9775022244ed7562852bec6c18
ivan.hoyos <ivan.hoyos@rappi.com>
2024-10-20 18:01:24 -0500

    Cloudwatch logic for monitoring and logs

--- main.tf
+++ main.tf
@@ -137,3 +137,10 @@ resource "aws_s3_bucket_notification" "thumbnail_notification" {
     aws_lambda_permission.thumbnail_allow_bucket
   ]
 }
+
+//Cloudwatch monitoring and logs
+resource "aws_cloudwatch_log_group" "thumbnail_cloudwatch_hg" {
+  name = "/aws/lambda/${aws_lambda_function.thumbnail_lambda.function_name}"
+
+  retention_in_days = 30
+}
